f3553a4b37c3fd59599775a1ccc4795f718f3ee5
madar-tmp <madar@iplstream.net>
2026-02-25 16:16:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,17 +2,18 @@
 
 # 1. Start a tiny web server in the background to satisfy Render's health check
 # Render gives you a port in the $PORT variable (usually 10000)
-python3 -m http.server ${PORT:-10000} &
+#python3 -m http.server ${PORT:-10000} &
 
 # 2. Start Tailscale daemon
-tailscaled --tun=userspace-networking --socks5-server=localhost:1080 &
-
+#tailscaled --tun=userspace-networking --socks5-server=localhost:1080 &
+sudo tailscaled --tun=userspace-networking &
 sleep 5
 
 # 3. Authenticate (Make sure you fixed the Auth Key as discussed!)
 tailscale up \
   --auth-key="${TAILSCALE_AUTHKEY}" \
   --hostname="${TAILSCALE_HOSTNAME:-render-vpn}" \
-  --advertise-exit-node
+  --advertise-exit-node \
+  --ssh
 
 wait
